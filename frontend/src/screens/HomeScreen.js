a438c15a794bb9bfe104a2efc6eb29b790fd488c
programer1983 <programer972@gmail.com>
2021-04-26 18:51:19 +0300

    Output of products in dynamics from the database;

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -19,12 +19,20 @@ const HomeScreen = () => {
             <h2 className="homescreen__title">Latest Products</h2>
 
             <div className="homescreen__products">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+                {loading ? (
+                <h2>Loading...</h2> 
+                ) : error ? (
+                <h2>{error}</h2> 
+                ) : (
+                 products.map((product) => <Product 
+                 key={product._id} 
+                 productId={product._id} 
+                 name={product.name}
+                 price={product.price}
+                 description={product.description}
+                 imageUrl={product.imageUrl}
+                 />)
+                )}
             </div>
         </div>
     )
