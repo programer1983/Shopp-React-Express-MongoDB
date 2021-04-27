ff93e214a4c16fe62ede331eb95258c24282ed52
programer1983 <programer972@gmail.com>
2021-04-27 20:15:22 +0300

    Add and remove in the basket, select the quantity in the product in the basket;

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,17 +1,39 @@
 import CartItem from '../components/CartItem'
 import './CartScreen.css'
+import {useDispatch, useSelector} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {useEffect} from 'react'
+import {addToCart, removeFromCart} from './../redux/actions/cartActions'
 
 
 const CartScreen = () => {
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+
+    const qtyChangeHandler = (id, qty) => {
+        dispatch(addToCart(id, qty))
+    }
+
+    const removeHandler = (id) => {
+        dispatch(removeFromCart(id))
+    }
+
+
     return (
         <div className="cartscreen">
             <div className="cartscreen__left">
                 <h2>Shopping Cart</h2>
-
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {cartItems.length === 0 ? (
+                    <div>
+                        You cart is empty <Link to="/">Go Back</Link>
+                    </div>
+                ) : cartItems.map((item) => (
+                    <CartItem item={item} 
+                              qtyChangeHandler={qtyChangeHandler} 
+                              removeHandler={removeHandler} />
+                ))}
             </div>
             <div className="cartscreen__right">
                 <div className="cartscreen__info">
